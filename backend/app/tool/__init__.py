3245000ba12c00b5f6f3dfcef324225be645f248
Haobo Yang <hobart.yang@qq.com>
2024-11-28 16:30:14 +0400

    update

--- backend/app/tool/__init__.py
+++ backend/app/tool/__init__.py
@@ -1,9 +1,13 @@
-from .searchOnlineTool import fetch_web_page, search_duckduckgo
-from .getStopsTool import get_all_stop_times
+# from .searchOnlineTool import fetch_web_page, search_duckduckgo
+# from .getStopsTool import get_all_stop_times
+from .generateMidiTool import convert_text_to_midi
+
+# TODO ADD TOOL LOAD CONTROL IN CONFIG FILE, AND MIGRATE TO HYDRA.
 
 FUNCTION_MAPPING = {
-    "fetch_web_page": fetch_web_page,
-    "search_duckduckgo": search_duckduckgo,
-    "get_all_stop_times": get_all_stop_times
+    # "fetch_web_page": fetch_web_page,
+    # "search_duckduckgo": search_duckduckgo,
+    # "get_all_stop_times": get_all_stop_times,
+    "convert_text_to_midi": convert_text_to_midi,
 }
 
